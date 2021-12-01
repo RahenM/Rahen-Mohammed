698fe36b0a1ffed799ac7f73fd4215a448c4c768
RahenM <rahenmqadaragha@yahoo.com>
2021-12-01 21:23:19 +0300

    just the name

--- SJF.java
+++ SJF.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SJF{//Rahen Mohammed Qader
+public class SJF{//Rahen Mohammed Qader Marroof
   public static void main(String[]args){
     Scanner sc=new Scanner (System.in);
     int a=4;
